8a11d4299441b185b1b6dd5461685e9dc9bd79a6
Derek Nofsinger <dereknofsinger@gmail.com>
2019-02-25 11:52:34 -0700

    Solved all test cases for "Balanced Brackets"

--- src/main/java/stacksAndQueues/BalancedBrackets.java
+++ src/main/java/stacksAndQueues/BalancedBrackets.java
@@ -11,34 +11,46 @@ public class BalancedBrackets {
     	
     	Stack<Character> charStack = new Stack<>();
     	
-    	//Go through half of the string, Java should round down on odd numbers.
-    	for(int i = 0; i < s.length() / 2; i++) {
-    		charStack.push(s.charAt(i));
-    	}
+    	//Go through the entire string and add '{', '(', and '[' to the stack.
+    	//If a '}', ')', and ']' show up, pop an item from the stack and verify that there is a matching bracket.
     	
-    	
-    	//Go through the second half of the string, let's pop off the characters from the stack
-    	//and see if they match.
-    	for(int j = s.length() / 2; j < s.length(); j++) {
+    	for(int i = 0; i < s.length(); i++) {
     		
-    		switch(s.charAt(j)) {
-    			case '}':
-    				if(charStack.pop() != '{')
-    					result = "NO";
-    				break;
-    			case ')':
-    				if(charStack.pop() != '(')
-    					result = "NO";
-    				break;
-    			case ']':
-    				if(charStack.pop() != '[')
-    					result = "NO";
-    				break;
+    		switch(s.charAt(i)) {
+    		
+    		case '{':
+				charStack.push(s.charAt(i));
+				break;
+			case '(':
+				charStack.push(s.charAt(i));
+				break;
+			case '[':
+				charStack.push(s.charAt(i));
+				break;
+    		
+			case '}':
+				if(charStack.empty() || charStack.pop() != '{')
+					result = "NO";
+				break;
+			case ')':
+				if(charStack.empty() || charStack.pop() != '(')
+					result = "NO";
+				break;
+			case ']':
+				if(charStack.empty() || charStack.pop() != '[')
+					result = "NO";
+				break;
     		}
-    		
+    	
     		if(result.equals("NO"))
     			break;
-
+    	}
+    	
+    	//Check to see if our stack is empty, if the stack isn't empty after looping through the string
+    	//then we have open brackets not matched.
+    	
+    	if(!charStack.empty()) {
+    		result = "NO";
     	}
     	
     	return result;
