2c032daecff2bbda3843b7a1a49f1894552909ff
Derek Nofsinger <dereknofsinger@gmail.com>
2019-02-28 13:48:17 -0700

    Added additional test for GraphTest for "Roads and Libraries"

--- src/test/java/GraphTest.java
+++ src/test/java/GraphTest.java
@@ -17,7 +17,7 @@ public class GraphTest {
 	}
 
 	@Test
-	public void roadsAndLibrariesTest() {
+	public void roadsAndLibraries1_Test() {
 		
 		//3 3 2 1
 		//Number of Cities: 3
@@ -32,6 +32,27 @@ public class GraphTest {
 		cost = Graph.roadsAndLibraries(3, 2, 1, roads);
 		
 		assertEquals(4, cost);
+	}
+	
+	@Test
+	public void roadsAndLibraries2_Test() {
+		
+		//6 6 2 5
+		//Number of Cities: 6
+		//Number of Roads: 6
+		//Cost of Library: 2
+		//Cost of Road: 5
+		//		1 3
+		//		3 4
+		//		2 4
+		//		1 2
+		//		2 3
+		//		5 6
+		long cost = 0;
+		int[][] roads = {{1, 3},{3, 4},{2, 4},{1, 2},{2, 3},{5, 6}};
+		cost = Graph.roadsAndLibraries(6, 2, 5, roads);
+		
+		assertEquals(12, cost);
 	}
 	
 
