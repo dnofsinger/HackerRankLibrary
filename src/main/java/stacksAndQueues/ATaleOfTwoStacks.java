7fc2ad9392ca920467425351043cf809a212e1a5
Derek Nofsinger <dereknofsinger@gmail.com>
2019-03-08 11:02:01 -0700

    Modified code to prevent timeouts.
    Adjusted code not to copy the entire enqueue stack until the dequeue
    stack is completelly empty.

--- src/main/java/stacksAndQueues/ATaleOfTwoStacks.java
+++ src/main/java/stacksAndQueues/ATaleOfTwoStacks.java
@@ -30,43 +30,30 @@ class MyQueue<T>{
 	Stack<T> enqueueStack = new Stack<>();
 	Stack<T> dequeueStack = new Stack<>();
 	
-	boolean pushMode = true;
-	
 	public void enqueue(T t) {
-		
-		
-		//We should check to see if we are in push mode, if we aren't
-		//then we need to transfer the dequeue stack to the enqueue stack.
-		if(!pushMode) {
-			while(!dequeueStack.empty()) {
-				enqueueStack.push(dequeueStack.pop());
-			}
-		}
-		
-		pushMode = true;
-		
+
 		enqueueStack.push(t);
 		
 	}
 	
 	public void createDequeue() {
 		
-		//Clear out the dequeue stack and start afresh. Instead of adding to existing stack.
+		//Should take O(N)
 		
 		while(!enqueueStack.empty()) {
 			dequeueStack.push(enqueueStack.pop());
 		}
-		
-		pushMode = false;
+
 	}
 	
 	public T dequeue() {
 		T t = null;
 		
-		createDequeue();
-		
-		if(!dequeueStack.isEmpty())
-			t = dequeueStack.pop();
+		if(dequeueStack.isEmpty())
+			createDequeue();
+			
+		//Should be O(1)
+		t = dequeueStack.pop();
 		
 		return t;
 	}
@@ -74,7 +61,9 @@ class MyQueue<T>{
 	public T peek() {
 		T t = null;
 		
-		createDequeue();
+		if(dequeueStack.isEmpty())
+			createDequeue();
+		
 		t = dequeueStack.peek();
 		
 		return t;
